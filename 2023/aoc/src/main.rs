a0b11b28d33f0b35bcc53ffd85919aa79b381540
tsusdere <tsusdere@github.com>
2023-12-19 19:06:30 -0600

    extract number game

--- 2023/aoc/src/main.rs
+++ 2023/aoc/src/main.rs
@@ -6,6 +6,14 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-    println!("{}", input);
+    for line in input.lines() {
+        let game = &line[0..line.find(':').unwrap_or(line.len())];
+        let parts: Vec<&str> = game.split(' ').collect();
+        if let Some(last_part) = parts.last() {
+            if let Ok(number) = last_part.parse::<i32>() {
+                println!("{}", number);
+            }
+        }
+    }
     0
 }
