ec9884de748567c3e95192e4bf008b48777509f1
tsusdere <tsusdere@github.com>
2023-12-19 18:39:17 -0600

    day 2

--- 2023/aoc/src/main.rs
+++ 2023/aoc/src/main.rs
@@ -1,46 +1,11 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
 
 fn main() {
-    let mut total = 0;
-    if let Ok(lines) = read_lines("input") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let numbers = extract_numbers(&ip);
-                // get the first number
-                let first = numbers[0];
-                // get the seond number if it exists else set it to 0
-                let second = if numbers.len() > 1 { numbers[1] } else { 0 };
-
-                if second == 0 {
-                    total += first * 11;
-                } else {
-                    let num = first.to_string() + &second.to_string();
-                    total += num.parse::<i32>().unwrap();
-                }
-            }
-        }
-    }
-    println!("Total: {}", total);
+    let input = read_to_string("input2").unwrap();
+    part1(&input);
 }
 
-// function to read line
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-// function to extract numbers
-fn extract_numbers(input: &str) -> Vec<i32> {
-    let mut numbers = Vec::new();
-    for c in input.chars() {
-        if let Some(digit) = c.to_digit(10) {
-            numbers.push(digit as i32);
-        }
-    }
-    numbers
+fn part1(input: &str) -> i32 {
+    println!("{}", input);
+    0
 }
